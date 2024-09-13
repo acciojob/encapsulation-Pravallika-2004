8dc5b5137492044243961522bac3dcc4f0ab89eb
Your Name <you@example.com>
2024-09-13 19:46:56 +0530

    updated changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+
+        //setting the name
+        rw.setName("sanjay");
+
+        //getting the name
+        System.out.println(rw.getName());
+    }
 }
